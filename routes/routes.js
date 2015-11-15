a28d79736a126aea0239a451ec532b34f3816246
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-14 22:23:26 -0500

    added basic routing to a views and search page

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,21 @@
 exports.init = function(app)
 {
     app.get("/", landing);
+    app.get("/search", search);
+    app.get("/recipes", recipes);
 }
 
 landing = function(request, response)
 {
     response.render("index");
+}
+
+search = function(request, response)
+{
+    response.render("search");
+}
+
+recipes = function(request, response)
+{
+    response.render("recipes");
 }
